c00fa4c9fed11984a593e19cb43c60d72e5b3f37
hmelioo <heliocenteiciv@gmail.com>
2025-06-11 20:28:50 -0400

    yeah

--- src/main/java/com/jayspers/jaysbuildadditions/item/custom/Switch2Item.java
+++ src/main/java/com/jayspers/jaysbuildadditions/item/custom/Switch2Item.java
@@ -1,11 +1,13 @@
 package com.jayspers.jaysbuildadditions.item.custom;
 
-import com.jayspers.jaysbuildadditions.procedures.Switch2ItemInHandTickProcedure;
+import com.jayspers.jaysbuildadditions.item.ModItems;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.entity.Entity;
+import net.neoforged.neoforge.items.ItemHandlerHelper;
 
 public class Switch2Item extends Item {
     public Switch2Item(Properties properties) {
@@ -14,9 +16,10 @@ public class Switch2Item extends Item {
 
     @Override
     public void inventoryTick(ItemStack itemstack, Level world, Entity entity, int slot, boolean selected) {
-        super.inventoryTick(itemstack, world, entity, slot, selected);
-        if (selected)
-            Switch2ItemInHandTickProcedure.execute(entity);
+        Player _player = (Player) entity;
+        ItemStack _setstack = new ItemStack(ModItems.LAWSUIT.get()).copy();
+        _setstack.setCount(1);
+        ItemHandlerHelper.giveItemToPlayer(_player, _setstack);
     }
 }
 
